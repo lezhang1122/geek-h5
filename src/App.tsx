fa0c7c9ac49cda0c2978e1c41c4b42c39fb87df5
zhangle.1122 <zhangle.1122@bytedance.com>
2022-03-06 17:58:08 +0800

    init router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import './App.scss'
-
+import { BrowserRouter as Router, Switch, Link, Route, Redirect } from 'react-router-dom';
+import './App.scss';
+import Layout from './pages/Layout/Layout';
+import Login from './pages/Login/Login';
 function App() {
-  return <div className="app"></div>
+  return (
+    <Router>
+      <div className="app">
+        <Link to="/home">跳转首页</Link>
+        <Link to="/login">跳转登陆页</Link>
+        <Switch>
+          <Redirect exact from="/" to="/home" />
+          <Route path="/home" component={Layout} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
