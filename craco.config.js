9cee8946a55935a073fdec41af5e728f69a5f990
zhangle.1122 <zhangle.1122@bytedance.com>
2022-03-07 19:37:18 +0800

    eslintrc && 1px问题

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,9 @@
-const path = require('path')
+const path = require('path');
+const pxToViewport = require('postcss-px-to-viewport');
+const vw = pxToViewport({
+  // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+  viewportWidth: 375,
+});
 
 module.exports = {
   // webpack 配置
@@ -7,8 +12,14 @@ module.exports = {
     alias: {
       // 约定：使用 @ 表示 src 文件所在路径
       '@': path.resolve(__dirname, 'src'),
-      // 约定：使用 @scss 表示 样式 文件所在路径
-      '@scss': path.resolve(__dirname, 'src', 'assets', 'styles')
+      // 约定：使用 @css 表示 样式 文件所在路径
+      '@css': path.resolve(__dirname, 'src', 'assets', 'styles'),
     },
   },
-}
+  // 这里补充style配置
+  style: {
+    postOptions: {
+      plugins: [vw],
+    },
+  },
+};
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './index.scss';
 import { Button } from 'antd-mobile';
 export default function Login() {
   return (
     <div>
       <Button color="primary">Button</Button>
+      <div className='hairline'>1px 细边框</div>
+      <div className='border1'>1px 正常边框</div>
     </div>
   );
 }
