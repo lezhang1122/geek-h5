ddff2ddf4cbbb5d5f0e11efc4a1fe59f9c2f0dee
zhangle.1122 <zhangle.1122@bytedance.com>
2022-03-08 16:25:40 +0800

    给types加上注释

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,14 +1,15 @@
+// 登陆表单
 export type LoginForm = {
   mobile: string;
   code: string;
 };
 
-// 泛型函数，接口，类
+// 响应数据
 export interface ApiResponse<T> {
   message: string;
   data: T;
 }
-
+// 用户token
 export type Token = {
   refresh_token: string;
   token: string;
--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -1,12 +1,14 @@
 import store from '@/store';
 import { ThunkAction } from 'redux-thunk';
+import { Token } from './data';
 
+// store
 export type RootState = ReturnType<typeof store.getState>;
+type RootAction = LoginAction;
+export type RootThunkAction = ThunkAction<void, RootState, unknown, RootAction>;
 
+// 登陆的action
 type LoginAction = {
   type: 'login/token';
-  payload: any;
+  payload: Token;
 };
-type RootAction = LoginAction;
-
-export type RootThunkAction = ThunkAction<void, RootState, unknown, RootAction>;
